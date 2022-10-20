62654780d91721b7c0cd77cf95bdf2c84e7457dd
karoline <karoline.wahl@hotmail.no>
2022-10-20 10:03:00 +0200

    Cartpole illustrated during training

--- cartpole_v1.py
+++ cartpole_v1.py
@@ -1,17 +1,24 @@
 import gym
 import numpy as np
 from sklearn.preprocessing import KBinsDiscretizer
-import time, math, random
-from typing import Tuple
+import math, time
 import warnings
 import tqdm
-import matplotlib.pyplot as plt
+
+
+'''
+Rules: 
+The only forces that can be applied are +1 and -1, which translates to a movement of either left or right. 
+If the cart moves more than 2.4 units from the center the episode is over. 
+If the angle is moved more than 15 degrees from the vertical the episode is over. 
+The reward is +1 for every timestamp that the episode is not over.
+'''
 
 environment = gym.make('CartPole-v1')
 
 #Q-table
 #Actions: left - right
-print(environment.action_space.n)
+print("Environment action space: ", environment.action_space.n)
 
 class CartPoleAgent:
     def __init__(self, env, episodes):
@@ -23,9 +30,9 @@ class CartPoleAgent:
         self.upper_bounds = [env.observation_space.high[2], math.radians(50)]
         self.lr = 0.1
         self.min_lr_rate = 0.1
-        self.min_explo_rate=0.1
+        self.min_explo_rate = 0.1 # Epsilon
         self.decay = 24
-        self.discount = 1.0
+        self.discount = 0.95 # Gamma -> should maybe be 0.9
         self.episodes = episodes
 
     #Gets dsicrete value since the original state is contionus (time)
@@ -34,6 +41,7 @@ class CartPoleAgent:
         est = KBinsDiscretizer(n_bins=self.n_bins, encode='ordinal', strategy='uniform')
         est.fit([self.lower_bounds, self.upper_bounds])
         return tuple(map(int, est.transform([[angle, pole_vel]])[0]))
+
     # Returns the policy used for getting the next action (highest value in q table next)
     def get_policy(self, state):
         return np.argmax(self.q_table[state])
@@ -42,11 +50,11 @@ class CartPoleAgent:
     def update_q_value(self, reward, state, action, new_state):
         return self.lr * (reward + self.discount * np.max(self.q_table[new_state]) - self.q_table[state][action])
 
-    #Adaptive lr. Used to adapt the learning rate
+    # Adaptive lr. Used to adapt the learning rate
     def get_lr(self, n):
         return max(self.min_lr_rate, min(1.0, 1.0 - math.log10((n+1) / self.decay)))
 
-    #Decaying explo rate/epsilon
+    #D ecaying explo rate/epsilon
     def get_explo_rate(self, n):
         return max(self.min_explo_rate, min(1.0, 1.0 - math.log10((n+1) / self.decay)))
 
@@ -56,6 +64,7 @@ class CartPoleAgent:
             current_state = self.get_discrete(self.env.reset())
             self.lr = self.get_lr(e)
             done = False
+
             #Tracks how many inputs it survives
             score = 0
             while not done:
@@ -66,6 +75,7 @@ class CartPoleAgent:
                     action = self.env.action_space.sample()
 
                 #After each step an observable state, reward and if it is done is returned
+                self.env.render()
                 obs, reward, done, _ = self.env.step(action)
                 #Discretes the new state
                 new_state = self.get_discrete(obs)
@@ -86,7 +96,7 @@ class CartPoleAgent:
         return scores
 
 warnings.filterwarnings('ignore')
-model = CartPoleAgent(environment, episodes=2000)
+model = CartPoleAgent(environment, episodes=1000) # Change to 10000
 scores = model.train()
 
 
