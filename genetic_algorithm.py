e15ae1258ed296b74b885b620af179c92d150129
karoline <karoline.wahl@hotmail.no>
2023-07-31 11:22:02 +0200

    Exercise 9 IDATT2502.

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,6 +1,7 @@
 import random
-
+import time
 from sklearn.utils import shuffle
+import matplotlib as plt
 
 # Initialize target
 target = 11001000 # 200
@@ -8,34 +9,42 @@ target = 11001000 # 200
 # Should be the same as the target
 result = 0
 
-# Initialize random population and put them into a list
-population = [1110011, 1100, 11, 11011, 1100101,               # 115, 12, 3, 27, 101,
-              11000111, 11111010, 11010011, 10111000, 1001000] # 199, 250, 211, 184, 72
+# Initialized at runtime
+population = []
 
 mut_prob_count = 0
+mut_count = 90
 
 
 # To be called inside of the main function
 def genetic_algorithm():
     global population
+    global result
     counter = 0
     # While target not reached (result is set in selection algorithm when an individual has fitness == 0)
     while (result == 0):
+        #print ("population before selection: ", population)
         # Fitness is measured through selection and the weakest are removed
         select() 
-        if (counter == 1000):
+        if (result != 0):
+            break
+        if (counter == 400):
             print("Randomizing")
             randomize()
             counter = 0
-
+        #print ("population after selection: ", population)
         # Combining the selected individuals "genes"
         new_gen = crossover()
-        # Mutate their genes (possibly)
-        new_gen = mutation(new_gen)
+
+        # Add two randomly generated bit strings
+        new_gen.append(random.getrandbits((len(target))))
+        new_gen.append(random.getrandbits((len(target))))
+
         # Apply the new generation to the population 
-        population.clear() 
         population = new_gen[:] 
         counter += 1
+       # if(counter == 10):
+          #  result = 1
 
     # Loop broken when target found
     return result
@@ -45,49 +54,57 @@ def genetic_algorithm():
 def fitness(individual):
     individual_dec = to_decimal(individual)
     target_dec = to_decimal(target)
+    #print("fitness: ", abs(individual_dec - target_dec))
 
-    return abs(individual_dec - target_dec) #Should be -abs
+    return -abs(individual_dec - target_dec) #Should be -abs
 
+def compare(bit1, bit2):
+    if (fitness(bit1) > fitness(bit2)):
+        return 1
+    elif (fitness(bit1) == fitness(bit2)):
+        return 0
+    else:
+        return -1
 
 def select():
     global population
     global result
-    strongest = []
-
+    #strongest = []
     best_fitness = 255
+    worst_fitness = 0
     total_fitness = 0
 
+    #print("population before selection: ", len(population))
+
+    # Sort population
+    sorted(population, key=lambda x: -fitness(x))
+    # Cut off the two worst
+    population = population[:-2] 
+
+    '''
     for i in range(len(population)):
-
-        current_fitness = fitness(population[i])
+        
+        current_fitness = abs(fitness(population[i]))
         total_fitness += current_fitness
-
-        if (current_fitness < best_fitness):
-            best_fitness = current_fitness
-
-        # If fit, the individual makes it past selection
-        if (current_fitness < 120):
-            strongest.append(population[i])
 
         # Algorithm termination base line
         if (current_fitness == 0):
             result = population[i]
             break 
-    
+
+        if (current_fitness < best_fitness):
+            best_fitness = current_fitness
+            strongest.insert(0, population[i])
+
+        else:
+            strongest.append(population[i])
+    '''
+
+    #TODO print("population after selection: ", len(population))
+
     # Print the best fitness for this generation
     print("Best fitness of the generation: ", best_fitness)
     print("Average fitness of the generation: ", total_fitness/(len(population)))
-
-    # Cut of the two weakest (the two first) elements and assign to the population
-    population.clear() 
-
-    # Cut of 10% (weakest individuals at the end of the list)
-    population = strongest[:] 
-
-    # If population is odd number, make it an even number
-    if (len(population) % 2 != 0):
-        population = population[:-1] # pop is slow when list is large
-
 
 # Crossover algorithm
 def crossover():
@@ -95,28 +112,36 @@ def crossover():
     global population
     new_generation = []
 
-    # Randomize the order of the population
-    random.shuffle(population)
+    shuffle(population)
 
-    for i in range (len(population)):
+    # Randomize the order of the population
+    # random.shuffle(population)
+
+    for i in range (0,len(population), 2):
+
+        if (mut_prob_count > mut_count):
+            new_generation = mutation(new_generation)
+            mut_prob_count = 0
+
         # Because each loop used i and i + 1, we skip every odd-i loop
-        if (i % 2 != 0): 
-            continue
+        #if (i % 2 != 0): 
+         #   continue
 
         # Select two individuals from the population
         mom = population[i]
         dad = population[i + 1]
-        
+
         # The mom and dad create a random number of children
-        amount_children = child_count()
+       # amount_children = child_count()
 
         # To not empty out of individuals in population
-        if (len(population) < 10):
-            amount_children = 4
+        #if (len(population) < 10):
+            #amount_children = 4
 
-        moms_genes = []
-        dads_genes = []
+        #moms_genes = []
+        #dads_genes = []
         
+        '''
         if (fitness(mom) > fitness(dad)):
             # If mom has the best fitness, then dad cannot be longer than mom
             if (length(dad) > length(mom)):
@@ -124,43 +149,24 @@ def crossover():
         elif (fitness(dad) > fitness(mom)):
             # If dad has the best fitness, then mom cannot be longer than dad
             if (length(mom) > length(dad)):
-
-
                 mom = str(mom)[:length(dad)] # Slice off yet again...
-            
+        '''
+
         # Two elements in each list (split method defined in script; not python method)
         moms_genes = split(mom) 
         dads_genes = split(dad) 
 
+        child1 = str(moms_genes[0]) + str(dads_genes[1])
+        child2 = str(dads_genes[0]) + str(moms_genes[1])
+      
+        new_generation.append(child1)
+        new_generation.append(child2)
 
-        if (amount_children == 1):
-            child = str(min(dads_genes)) + str(max(moms_genes)) 
-            new_generation.append(int(child))
-            mut_prob_count = mut_prob_count + 1
-        elif (amount_children == 2):
-            child1 = str(min(moms_genes)) + str(max(dads_genes)) 
-            child2 = str(min(dads_genes)) + str(max(moms_genes))
-            new_generation.append(int(child1))
-            new_generation.append(int(child2))
-            mut_prob_count = mut_prob_count + 2
-        elif (amount_children == 3):
-            child1 = str(min(moms_genes)) + str(max(dads_genes))
-            child2 = str(min(dads_genes)) + str(max(moms_genes))
-            child3 = str(min(dads_genes)) + str(min(moms_genes))
-            new_generation.append(int(child1))
-            new_generation.append(int(child2))
-            new_generation.append(int(child3))
-            mut_prob_count = mut_prob_count + 3
-        else:
-            child1 = str(min(moms_genes)) + str(max(dads_genes))
-            child2 = str(min(dads_genes)) + str(max(moms_genes))
-            child3 = str(min(dads_genes)) + str(min(moms_genes))
-            child4 = str(max(dads_genes)) + str(max(moms_genes))
-            new_generation.append(int(child1))
-            new_generation.append(int(child2))
-            new_generation.append(int(child3))
-            new_generation.append(int(child4))
-            mut_prob_count = mut_prob_count + 4
+        mut_prob_count += mut_prob_count + 2
+
+        if (mut_prob_count > mut_count):
+            new_generation = mutation(new_generation)
+            mut_prob_count = 0
 
     # Return the new generation
     return new_generation
@@ -170,28 +176,20 @@ def crossover():
 def mutation(new_generation):
     global mut_prob_count
 
-    # 1% Chance of mutation
-    if (mut_prob_count >= 100):
-        # The element from 'new_generation' that we will mutate
-        element = 0
-        children_since = mut_prob_count - 100
-        if (children_since == 0): # mut_prob_count = 100
-            element = new_generation[-1]
-            element = bit_flip(element) 
-            new_generation.insert(len(new_generation), element) # Switch in the new, mutated individual at the correct pos 
-        else: # mut_prob_count > 100
-            element = new_generation[-children_since] # Mutate the correct individual
-            element = bit_flip(element) 
-            new_generation.insert(len(new_generation) - children_since, element) 
-
-        # Reset probability count
-        mut_prob_count = 0
+    # The element from 'new_generation' that we will mutate
+    element = new_generation[-1]
+    element = bit_flip(element) 
+    new_generation.insert(len(new_generation), element) # Switch in the new, mutated individual at the correct pos 
+    
+    # Reset probability count
+    mut_prob_count = 0
 
     # Return the list, mutated individuals or not
     return new_generation
 
 # Randomizes half of the population with different bit lengths
 def randomize():
+    global population
     new_bits = []
     iter_size = 0
     half_target_len = length(target)/2
@@ -207,15 +205,15 @@ def randomize():
     # Generates decimal number with a specified bit length (if converted to bits)
     for i in range (int(iter_size)):
         rand_nr = random.getrandbits(int(half_target_len))
-        while (rand_nr == 0 or rand_nr == 1):
-            rand_nr = random.getrandbits(int(half_target_len))
+        #while (rand_nr == 0 or rand_nr == 1):
+        #    rand_nr = random.getrandbits(int(half_target_len))
 
         new_bits.append(get_bin(rand_nr))
 
     for i in range (int(iter_size)):
         rand_nr = random.getrandbits(length(target))
-        while (rand_nr == 0 or rand_nr == 1):
-            rand_nr = random.getrandbits(int(half_target_len))
+        #while (rand_nr == 0 or rand_nr == 1):
+        #    rand_nr = random.getrandbits(int(half_target_len))
 
         new_bits.append(get_bin(rand_nr))
     
@@ -223,10 +221,6 @@ def randomize():
 
     # Add to the population
     population[:len(new_bits)] = new_bits
-
-
-# Decimal to binary number lambda function
-get_bin = lambda x: format(x, 'b')
 
 
 # Support method for mutation alg
@@ -237,6 +231,7 @@ def bit_flip(individual):
     digits = length(individual) 
     random_bit = random.randint(1, digits) 
     bit = str_individual[random_bit - 1] # -1 for correct indexation
+    global target
 
     if (bit == 1):
         str_individual[random_bit - 1] = '0' 
@@ -246,7 +241,7 @@ def bit_flip(individual):
     # Back to string before converting to int
     str_individual = ''.join(str_individual)
 
-    return int(str_individual)
+    return str_individual
 
 # Supporting method for crossover algorithm that returns a bit sequence in the given length
 def split(parent):
@@ -256,7 +251,8 @@ def split(parent):
     # Specialcase of bit-representation of decimal numbers 0 and 1
     if (length(parent) == 1):
         # Both children will inherit the single bit from this parent
-        split_parent == [parent, parent] 
+        split_parent.append(parent)
+        split_parent.append(parent)
         return split_parent
 
     # If even number > 1
@@ -276,22 +272,42 @@ def split(parent):
 #TODO TypeError: slice indices must be integers or None or have an __index__ method
 # Supporting method for crossover algorithm
 def child_count():
-    # Two individuals can have 1-4 children 
-    return random.randint(1,4)
+    # Two individuals can have 1-2 children 
+    return random.randint(1,2)
 
 # Supporting method for fitness algorithm
 def to_decimal(bits):
     # From bits to decimal 
     return int(str(bits), 2)
 
+# Decimal to binary number lambda function
+get_bin = lambda x: format(x, 'b')
+
 # Support method that finds the length of a number
 def length(number):
     return len(str(number)) 
 
+def init_population():
+    global population
+    new_population = []
 
+    # Randomize the population to a count of 10
+    for i in range (10):   
+        individual = random.getrandbits(len(target)) 
+        new_population.append(get_bin(individual))
+    population = new_population[:]
 
 
 #################################################################################################################
+
+'''
+# Randomize the population
+for i in range (50):   
+    rand_length = random.randint(1,8)
+    individual = random.getrandbits(rand_length) # Might have to insert while loop to remove 0's
+    #while(individual == 0):
+    #    individual = random.getrandbits(rand_length)
+    population.append(get_bin(individual))
 
 bit_res = genetic_algorithm()
 print("The result was: \n", bit_res)
@@ -299,3 +315,49 @@ if(bit_res == target):
     print("Which is equal to the target!")
 else:
     print("Which is not equal to the target, because the target was: \n", target)
+'''
+
+# For 8 iterations, create a random bit-sequence (with iteratively larger bit length -> +1) 
+y_axis = []
+for i in range (10): 
+    bit_sequence = random.getrandbits(i + 1)
+    binary = get_bin(bit_sequence)
+
+    # To be able to predict the length based on the loop
+    while (length(binary) != (i + 1)):
+        bit_sequence = random.getrandbits(i + 1)
+        binary = get_bin(bit_sequence)
+
+    # Assign the target
+    target = binary
+
+    # Randomize the population
+    init_population()
+
+    runs = 3
+    # Total time for all n runs
+    tot_time = 0
+    # For 3 iterations 
+    for i in range (runs):
+        # Record the time the algorithm runs each time
+        print("Target is: ", target)
+        start_time = time.time()
+        res = genetic_algorithm()
+        end_time = time.time()
+        tot_time += end_time-start_time
+        
+        print("Elapsed time: ", end_time - start_time)
+        print("Result was: ", res)
+        result = 0 # Reset the result variable
+        init_population() # New population
+        break
+    
+    avg_time = tot_time/runs
+    y_axis.append(avg_time)
+
+# Line diagram    
+plt.plot(range(10), y_axis)
+plt.show()
+# Plot bit length (bit sequence) to the average time using a line diagram
+# x-axis = bit length
+# y-axis = avg_time
