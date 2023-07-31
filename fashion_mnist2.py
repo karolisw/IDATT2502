06dc7dce97096390c88233acf145442ad5781023
karoline <karoline.wahl@hotmail.no>
2023-07-31 11:16:05 +0200

    Exercise 3 IDATT2502.

---
+++ fashion_mnist2.py
@@ -0,0 +1,102 @@
+# Based on the nn file 
+import torch
+import torch.nn as nn
+import torchvision
+
+# Making the code device-agnostic
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+ 
+# Load observations from the mnist dataset. The observations are divided into a training set and a test set
+mnist_train = torchvision.datasets.FashionMNIST('./data', train=True, download=True)
+x_train = mnist_train.data.reshape(-1, 1, 28, 28).float() # torch.functional.nn.conv2d argument must include channels (1)
+y_train = torch.zeros((mnist_train.targets.shape[0], 10)) # Create output tensor 
+y_train[torch.arange(mnist_train.targets.shape[0]), mnist_train.targets] = 1  # Populate output
+
+mnist_test = torchvision.datasets.FashionMNIST('./data', train=False, download=True)
+x_test = mnist_test.data.reshape(-1, 1, 28, 28).float()  # torch.functional.nn.conv2d argument must include channels (1)
+y_test = torch.zeros((mnist_test.targets.shape[0], 10))  # Create output tensor
+y_test[torch.arange(mnist_test.targets.shape[0]), mnist_test.targets] = 1  # Populate output
+
+# Normalization of inputs
+mean = x_train.mean()
+std = x_train.std()
+x_train = (x_train - mean) / std
+x_test = (x_test - mean) / std
+
+# Putting x_train and y_train onto the GPU
+x_train = x_train.to(device)
+y_train = y_train.to(device)
+
+# Putting x_test and y_test onto the GPU
+x_test = x_test.to(device)
+y_test = y_test.to(device)
+
+# Divide training data into batches to speed up optimization
+batches = 600
+x_train_batches = torch.split(x_train, batches)
+y_train_batches = torch.split(y_train, batches)
+
+
+class ConvolutionalNeuralNetworkModel(nn.Module):
+
+    def __init__(self):
+        super(ConvolutionalNeuralNetworkModel, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+        self.drop = nn.Dropout(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10)
+
+
+    def logits(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
+
+    # Predictor
+    def f(self, x):
+        return torch.softmax(self.logits(x), dim=1)
+
+    # Cross Entropy loss
+    def loss(self, x, y):
+        return nn.functional.cross_entropy(self.logits(x), y.argmax(1))
+
+    # Accuracy
+    def accuracy(self, x, y):
+        return torch.mean(torch.eq(self.f(x).argmax(1), y.argmax(1)).float())
+
+
+model = ConvolutionalNeuralNetworkModel()
+
+# Putting the model onto the GPU
+model = model.to(device)
+
+# Optimize: adjust W and b to minimize loss using stochastic gradient descent
+optimizer = torch.optim.Adam(model.parameters(), 0.01)
+for epoch in range(50):
+    for batch in range(len(x_train_batches)):
+        model.loss(x_train_batches[batch], y_train_batches[batch]).backward()  # Compute loss gradients
+        optimizer.step()  # Perform optimization by adjusting W and b,
+        optimizer.zero_grad()  # Clear gradients for next step
+
+    print("accuracy = %s" % model.accuracy(x_test, y_test))
